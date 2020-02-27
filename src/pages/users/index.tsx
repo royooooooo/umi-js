67a36bfab3f9220f0c4db51d6fdf2cc2d40fe1df
Fu Yi <yifu@thoughtworks.com>
2020-02-27 17:40:37 +0800

    add user login check.

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { UserState } from '@/models/user-info';
 import { connect } from 'dva';
+import { router } from 'umi';
 
 const WelcomePage = ({ userName }: { userName: string }) => {
+  if (!userName) {
+    router.push("/users/login");
+  }
   return <div>hello user:{userName}</div>;
 };
 
